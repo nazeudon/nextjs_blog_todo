b891973cfe20653dea01da59b0d0f8871a5c13d2
nazeudon <nazeudon48112@icloud.com>
2021-05-20 12:23:22 +0900

    added auth component functions

--- components/Auth.js
+++ components/Auth.js
@@ -1,4 +1,73 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Cookie from "universal-cookie";
+
+const cookie = new Cookie();
+
 export default function Auth() {
+  const router = useRouter();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
+
+  const login = async () => {
+    try {
+      console.log(process.env.NEXT_PUBLIC_RESTAPI_URL);
+      await fetch(
+        `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/auth/jwt/create/`,
+        {
+          method: "POST",
+          body: JSON.stringify({ username: username, password: password }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((res) => {
+          if (res.status === 400) {
+            throw "authentication failed";
+          } else if (res.ok) {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          const options = { path: "/" };
+          cookie.set("access_token", data.access, options);
+        });
+      router.push("/main-page");
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  const register = async () => {
+    try {
+      await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/register/`, {
+        method: "POST",
+        body: JSON.stringify({ username: username, password: password }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((res) => {
+        if (res.status === 400) {
+          throw "authentication failed";
+        }
+      });
+      login();
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  const authUser = async (e) => {
+    e.preventDefault();
+    if (isLogin) {
+      login();
+    } else {
+      register();
+    }
+  };
+
   return (
     <>
       <div className="max-w-md w-full space-y-8">
@@ -9,32 +78,34 @@ export default function Auth() {
             alt="Workflow"
           />
           <h2 className="mt-6 text-center text-3xl font-extrabold text-white-900">
-            Sign in to your account
+            {isLogin ? "Login" : "Signup"}
           </h2>
         </div>
-        <form className="mt-8 space-y-6" action="#" method="POST">
+        <form className="mt-8 space-y-6" onSubmit={authUser}>
           <input type="hidden" name="remember" value="true" />
           <div className="rounded-md shadow-sm -space-y-px">
             <div>
               <input
-                id="email-address"
-                name="email"
-                type="email"
-                autoComplete="email"
+                name="username"
+                type="text"
+                autoComplete="username"
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Email address"
+                placeholder="User name"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
               />
             </div>
             <div>
               <input
-                id="password"
                 name="password"
                 type="password"
                 autoComplete="current-password"
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
           </div>
@@ -42,6 +113,7 @@ export default function Auth() {
           <div className="flex items-center justify-center">
             <div className="text-sm">
               <span
+                onClick={() => setIsLogin(!isLogin)}
                 href="#"
                 className="cursor-pointer font-medium text-white hover:text-indigo-500"
               >
@@ -70,7 +142,7 @@ export default function Auth() {
                   />
                 </svg>
               </span>
-              Sign in
+              {isLogin ? "Login with JWT" : "Create new user"}
             </button>
           </div>
         </form>
