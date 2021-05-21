1cd2cc946e4d5bcaa04b3f128c2b7162ea9abedf
nazeudon <nazeudon48112@icloud.com>
2021-05-21 22:42:32 +0900

    added posts detail and ISR

--- components/Post.js
+++ components/Post.js
@@ -1,11 +1,15 @@
+import Link from "next/link";
+
 export default function Post({ post }) {
   return (
     <div>
       <span>{post.id}</span>
       {" : "}
-      <span className="text-white border-b border-gray-500 cursor-pointer hover:bg-gray-600">
-        {post.title}
-      </span>
+      <Link href={`/posts/${post.id}`}>
+        <span className="text-white border-b border-gray-500 cursor-pointer hover:bg-gray-600">
+          {post.title}
+        </span>
+      </Link>
     </div>
   );
 }
