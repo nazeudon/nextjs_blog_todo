1f4e727840528d3a25e92ecc9d712729f789832a
nazeudon <nazeudon48112@icloud.com>
2021-05-23 22:04:28 +0900

    added StateContext

--- pages/task-page.js
+++ pages/task-page.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import Link from "next/link";
 import useSWR from "swr";
 import { getAllTasksData } from "../lib/tasks";
+import StateContextProvider from "../context/StateContext";
 import Layout from "../components/Layout";
 import Task from "../components/Task";
 
@@ -21,33 +22,35 @@ export default function TaskPage({ staticfilteredTasks }) {
   }, []);
 
   return (
-    <Layout title="Task page">
-      <ul>
-        {filteredTasks &&
-          filteredTasks.map((task) => (
-            <Task key={task.id} task={task} taskDeleted={mutate} />
-          ))}
-      </ul>
-      <Link href="/main-page">
-        <div className="flex mt-12 cursor-pointer">
-          <svg
-            className="w-6 h-6 mr-3"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
-            />
-          </svg>
-          <span>Back to main page</span>
-        </div>
-      </Link>
-    </Layout>
+    <StateContextProvider>
+      <Layout title="Task page">
+        <ul>
+          {filteredTasks &&
+            filteredTasks.map((task) => (
+              <Task key={task.id} task={task} taskDeleted={mutate} />
+            ))}
+        </ul>
+        <Link href="/main-page">
+          <div className="flex mt-12 cursor-pointer">
+            <svg
+              className="w-6 h-6 mr-3"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+              />
+            </svg>
+            <span>Back to main page</span>
+          </div>
+        </Link>
+      </Layout>
+    </StateContextProvider>
   );
 }
 
